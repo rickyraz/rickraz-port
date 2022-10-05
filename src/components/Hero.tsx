eb20ac3663e1a9c8b05ed75365915a6a1e08e78d
rickyrazhari <rickyraihan83@gmail.com>
2022-10-05 14:14:33 +0700

    update-img

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,11 +32,11 @@ const Header = () => {
 
 const Main = ({ title }) => {
   return (
-    <section class="mt-64 flex flex-col space-y-2">
-      <h1 class="font-sora text-5xl font-bold uppercase text-slate-800">
+    <section class="mt-auto mb-10 flex flex-col space-y-2 text-center md:mt-auto md:mb-16 md:text-left">
+      <h1 class="font-sora text-3xl font-bold uppercase text-slate-800 md:text-5xl">
         {title}
       </h1>
-      <p class="font-inter text-4xl font-normal uppercase text-slate-400">
+      <p class="font-inter text-3xl font-normal uppercase text-slate-400 md:text-4xl">
         - CLP, Indonesia
       </p>
     </section>
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,10 +1,9 @@
 import { h, Fragment } from "preact";
 import { useRef, useState, useEffect } from "preact/hooks";
-import { Tab } from "@headlessui/react";
 
 export default function Project() {
   return (
-    <section class="mx-auto box-border max-w-[1284px] px-4 font-inter md:px-0">
+    <section class="mx-auto mb-16 box-border max-w-[1284px] px-4 font-inter md:mb-24 md:px-0">
       <hr class="border-t-[3px] border-t-slate-200" />
       <div class="mt-12 flex h-10 items-center justify-between">
         <h2 class=" text-2xl font-semibold uppercase text-slate-700 md:text-[32px]">
@@ -12,27 +11,30 @@ export default function Project() {
         </h2>
         <SegmentedControl />
       </div>
-      <div class="mt-8 grid gap-6 md:mt-12 md:grid-cols-2 md:gap-12">
+      <div class="mt-8 grid gap-8 md:mt-12 md:grid-cols-2 md:gap-12">
         <Box
           title="mini app"
           src="/mini.webp"
           alt="miniapp"
           year="2021"
           lang2="Javascript"
+          lang1="Typescript"
         />
         <Box
           title="tesla clone"
-          src="/"
+          src="/tesla.webp"
           alt="teslaclone"
           year="2021"
           lang1="Typescript"
+          link="https://tesla-clone-v2.vercel.app/ "
         />
         <Box
-          title="Real Estate"
-          src="/"
+          title="Real Estate v1"
+          src="/real-v1.webp"
           alt="realestate"
           year="2022"
           lang1="Typescript"
+          link="https://rickyraz-real-estate-v1.vercel.app/"
         />
       </div>
     </section>
@@ -60,53 +62,65 @@ type BoxProps = {
   lang1?: string;
   lang2?: string;
   lang3?: string;
+  link?: string;
 };
 
-const Box = ({ title, year, src, alt, lang1, lang2, lang3 }: BoxProps) => {
+const Box = ({
+  title,
+  year,
+  src,
+  alt,
+  lang1,
+  lang2,
+  lang3,
+  link,
+}: BoxProps) => {
   return (
     <div class="group transform transition-all duration-300 hover:-translate-y-1">
-      <div class=" relative box-border overflow-hidden rounded-lg border-2 border-slate-200/40 bg-white shadow-sm transition-all duration-300 group-hover:rounded-3xl">
-        <div class="min-h-[180px] p-3 md:h-[360px] md:p-5">
-          <img
-            src={src}
-            alt={alt}
-            class="h-full w-full rounded-xl bg-slate-200 object-cover transition-all duration-300 group-hover:rounded-3xl"
-          />
-        </div>
-        <div class="mx-3 flex justify-between pb-3 md:mx-5 md:pb-5">
-          <div>
-            <p class="text-sm font-semibold text-slate-400 md:text-[15px]">
-              {year}
-            </p>
-            <h3 class="pt-1 font-sora text-xl font-bold capitalize text-slate-700 md:pt-2 md:text-2xl">
-              {title}
-            </h3>
+      <a href={link} target="blank">
+        <div class=" relative box-border overflow-hidden rounded-lg border-2 border-slate-200/40 bg-white shadow-sm transition-all duration-300 group-hover:rounded-3xl">
+          <div class="min-h-[180px] p-3 md:h-[360px] md:p-5">
+            <img
+              src={src}
+              alt={alt}
+              class="h-full w-full rounded-xl bg-slate-200 object-cover transition-all duration-300 group-hover:rounded-3xl"
+            />
           </div>
-          <ul class="flex space-x-3">
-            {lang1 && (
-              <li>
-                <a class="rounded-full bg-[#0A84FF] py-2 px-4 text-sm font-medium text-white">
-                  {lang1}
-                </a>
-              </li>
-            )}
-            {lang2 && (
-              <li>
-                <a class="rounded-full bg-[#FDBC14] py-2 px-4 text-sm font-medium text-white">
-                  {lang2}
-                </a>
-              </li>
-            )}
-            {lang3 && (
-              <li>
-                <a class="rounded-full bg-[#FFA53B] py-2 px-4 text-sm font-medium text-white">
-                  {lang3}
-                </a>
-              </li>
-            )}
-          </ul>
+          <div class="mx-3 flex justify-between pb-3 md:mx-5 md:pb-5">
+            <div>
+              <p class="text-sm font-semibold text-slate-400 md:text-[15px]">
+                {year}
+              </p>
+              <h3 class="pt-1 font-sora text-xl font-bold capitalize text-slate-700 md:pt-2 md:text-2xl">
+                {title}
+              </h3>
+            </div>
+            <ul class="flex space-x-3">
+              {lang1 && (
+                <li>
+                  <a class="rounded-full bg-[#0A84FF] py-[6px] px-3 text-[12px] font-medium text-white md:px-4 md:py-2">
+                    {lang1}
+                  </a>
+                </li>
+              )}
+              {lang2 && (
+                <li>
+                  <a class="rounded-full bg-[#FDBC14] py-[6px] px-3 text-[12px] font-medium text-white md:px-4 md:py-2">
+                    {lang2}
+                  </a>
+                </li>
+              )}
+              {lang3 && (
+                <li>
+                  <a class="rounded-full bg-[#FFA53B] py-[6px] px-3 text-[12px] font-medium text-white md:px-4 md:py-2">
+                    {lang3}
+                  </a>
+                </li>
+              )}
+            </ul>
+          </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 };
